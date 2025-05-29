a5e20b9172e4518f9211da199ae0fcc1e7b812f9
darmond <bardiarezaeikalantari@gmail.com>
2025-05-29 19:18:31 +0330

    refactor: make the project clean and remove unnecessary files

--- Terraform/terraform_devopshobbies_tutorial/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-ami_id        = "ami-somerandomcharacters"
-instance_type = "t2.micro"
-key_name      = "dvh"
-machine_name  = "bardia"
--- Terraform/terraform_devopshobbies_tutorial/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "ami_id" {
-  type        = string
-  description = "this is an ami id for our machine in AWS"
-}
-variable "instance_type" {}
-variable "key_name" {}
-variable "machine_name" {}
