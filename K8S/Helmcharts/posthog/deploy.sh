d39f1801838804be0f61c895ae5184459b34d656
bardia.rezaei <bardia.rezaei@hamravesh.ir>
2026-02-09 13:11:58 +0330

    feat: add posthog

--- K8S/Helmcharts/posthog/deploy.sh
+++ K8S/Helmcharts/posthog/deploy.sh
@@ -33,46 +33,6 @@ echo '{\"date\": \"'"$(date +%Y-%m-%d)"'\"}' > ./share/GeoLite2-City.json;
 chmod 644 ./share/GeoLite2-City.mmdb &&
 chmod 644 ./share/GeoLite2-City.json
 
-rm -rf compose
-mkdir -p compose
-cat > compose/start <<EOF
-#!/bin/bash
-./compose/wait
-./bin/migrate
-./bin/docker-server
-EOF
-chmod +x compose/start
-
-cat > compose/temporal-django-worker <<EOF
-#!/bin/bash
-./bin/temporal-django-worker
-EOF
-chmod +x compose/temporal-django-worker
-
-# write wait script
-cat > compose/wait <<EOF
-#!/usr/bin/env python3
-
-import socket
-import time
-
-def loop():
-    print("Waiting for ClickHouse and Postgres to be ready")
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(('clickhouse', 9000))
-        print("Clickhouse is ready")
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(('db', 5432))
-        print("Postgres is ready")
-    except ConnectionRefusedError as e:
-        time.sleep(5)
-        loop()
-
-loop()
-EOF
-chmod +x compose/wait
-
 # start up the stack
 echo "Configuring Docker Compose...."
 rm -f docker-compose.yml
